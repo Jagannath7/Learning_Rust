bd3f241cc4787e86fbcf2db86f8b0659a8a44dae
JJ <jjcancode7@gmail.com>
2024-01-18 19:43:09 -0500

    Guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,18 +1,44 @@
 use std::io; // io is not a part of the prelude.
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-   println!("Guess the number");
+    println!("Guess the number");
 
-   println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-   let mut guess = String::new();
+    // println!("The secret number is {secret_number}");
 
-   io::stdin()  // std::io::stdin() without using use std::io.
-       .read_line(&mut guess)
-       .expect("Failed to read line");
-       
-   println!("You guessed: {guess}");
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+    
+        io::stdin()  // std::io::stdin() without using use std::io.
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            }
+       };
+           
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
 }
+
 
 // Rust variables are immutable by default.
 // let apples = 5; // immutable
